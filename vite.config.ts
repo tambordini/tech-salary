236e2ec0534beeef342add80596bd5bfb2dd3aa7
Tam Bordini <bordin@odds.team>
2024-12-24 11:50:29 +0700

    Add GitHub Actions workflow for deployment to GitHub Pages and update Vite config for base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/tech-salary/',
   plugins: [svelte()],
   server: {
     open: true,
