1558ec2dbdbcab4aed99bad88c72e7a0a15d52a5
Tam Bordini <bordin@odds.team>
2025-01-28 11:57:37 +0700

    feat: Add loading component and implement loading state in the main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<h2>Hello World</h2>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import Loading from '$lib/components/Loading.svelte';
+	import { onMount } from 'svelte';
+
+	let isLoading = true;
+
+	onMount(() => {
+		setTimeout(() => {
+			isLoading = false;
+		}, 1500);
+	});
+</script>
+
+{#if isLoading}
+	<Loading />
+{:else}
+	<h1>Welcome to SvelteKit</h1>
+	<h2>Hello World</h2>
+	<p>
+		Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
+	</p>
+{/if}
